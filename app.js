2500c7c0d173f1ad2358ca556088066032955f89
Sankalp Ramteke <141501439+Sankalp70@users.noreply.github.com>
2024-03-07 21:27:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,11 +14,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL,process.env.FRONTEND_URL_2],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+  cors()
 );
 
 app.get("/", (req, res) => {
